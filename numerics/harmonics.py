1e3d536f8c4432c7614ea862a1a8353d6c945ab7
Matthew Hagan <matthewhagan15@gmail.com>
2022-06-30 13:06:44 -0400

    added in harmonics, started eigenvecs

---
+++ numerics/harmonics.py
@@ -0,0 +1,51 @@
+from turtle import shape
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import linalg
+
+def thermal_state(hamiltonian, beta):
+    mat = linalg.expm(- beta * hamiltonian)
+    partition = np.trace(mat)
+    return mat / partition
+
+def harmonic_oscillator_hamiltonian(dimensions, gap = 1.0):
+    diags = [0.5 + n for n in range(dimensions)]
+    return np.diag(diags)
+
+def gue(dimensions):
+    raw = np.random.normal(scale = np.sqrt(2)/2, size=(dimensions,dimensions * 2)).view(np.complex128)
+    return (raw + raw.T.conj())/ np.sqrt(2)
+
+def partrace(mat, d1, d2):
+    tensor = mat.reshape([d1,d2,d1,d2])
+    return np.trace(tensor, axis1=1, axis2=3)
+
+def channel(beta1, beta2, time, alpha, dim1=10, dim2=10):
+    oscillator1 = harmonic_oscillator_hamiltonian(dim1)
+    oscillator2 = harmonic_oscillator_hamiltonian(dim2)
+    rho = np.kron( thermal_state(oscillator1, beta1), thermal_state(oscillator2, beta2))
+    interactions = gue(dim1 * dim2)
+    ham = np.kron(oscillator1, np.identity(dim2)) + np.kron(np.identity(dim1), oscillator2) + alpha * interactions
+    u = linalg.expm(1j * ham * time)
+    raw = u @ rho @ u.conj().T
+    return partrace(raw, dim1, dim2)
+
+def test():
+    d1 = 4
+    d2 = 8
+    b1 = 0.
+    b2 = 100.
+    t = 1.
+    alpha = 0.1
+    out = channel(b1, b2, t, alpha, dim1=d1, dim2=d2)
+    h1 = harmonic_oscillator_hamiltonian(d1)
+    therm1 = thermal_state(h1, b1)
+    print("oscillator 1")
+    print(h1)
+    print("output")
+    print(out)
+    print("#" * 50)
+    print("output diagonals:", np.abs(np.diagonal(out)))
+    print("input thermal state:", np.abs(np.diagonal(therm1)))
+    print("delta:", np.abs(np.diagonal(out)) - np.abs(np.diagonal(therm1)))
+test()
