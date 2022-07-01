d97b8658f5010ab48462963a78c237da12bb60a1
Matthew Hagan <matthewhagan15@gmail.com>
2022-07-01 00:46:55 -0400

    working class numerics

--- numerics/harmonics.py
+++ numerics/harmonics.py
@@ -1,17 +1,14 @@
+from audioop import avg
+from cProfile import label
+from tabnanny import verbose
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy import linalg
 from skopt import gbrt_minimize
 from skopt.space import Real
+import time as time_this
 
-def thermal_state(hamiltonian, beta):
-    mat = linalg.expm(- beta * hamiltonian)
-    partition = np.trace(mat)
-    return mat / partition
-
-def harmonic_oscillator_hamiltonian(dimensions, gap = 1.0):
-    diags = [0.5 + n for n in range(dimensions)]
-    return np.diag(diags)
+BETA_MAX = 1000
 
 def gue(dimensions):
     raw = np.random.normal(scale = np.sqrt(2)/2, size=(dimensions,dimensions * 2)).view(np.complex128)
@@ -20,6 +17,126 @@ def gue(dimensions):
 def partrace(mat, d1, d2):
     tensor = mat.reshape([d1,d2,d1,d2])
     return np.trace(tensor, axis1=1, axis2=3)
+
+def thermal_state(hamiltonian, beta):
+    mat = linalg.expm(- beta * hamiltonian)
+    partition = np.trace(mat)
+    ret = 0
+    try:
+        ret = mat / partition
+    except Exception as e:
+        print("BIG ERROR:")
+        print(e)
+        ret = mat
+    return ret
+
+def harmonic_oscillator_hamiltonian(dimensions, gap = 1.0):
+    diags = [0.5 + n for n in range(dimensions)]
+    return np.diag(diags)
+
+# returns (beta, fro error)
+def find_best_beta(rho, ham, verbose=False):
+    dims = [Real(0.0, BETA_MAX)]
+    def obj_fun(dim):
+        beta = dim[0]
+        # print("beta:", beta)
+        # print("matrix:")
+        # print(linalg.expm(-beta * ham))
+        guess = thermal_state(ham, beta)
+        return np.linalg.norm(rho - guess)
+    ret = gbrt_minimize(obj_fun, dims, x0=[1.0], verbose=verbose)
+    return (ret.x, ret.fun)
+
+class QHMC:
+    def __init__(
+                self,
+                env_betas = [1.0],
+                sys_start_beta = 0.0,
+                sim_times = [1],
+                alphas = [0.01],
+                used_env_per_beta = 1,
+                ham_sys=np.zeros((2,2)),
+                ham_env_base=np.diag([0.5, 1.5]),
+                num_monte_carlo=10,
+                verbose=False
+                ):
+        if len(env_betas) != len(sim_times) or len(sim_times) != len(alphas):
+            print("[QHMC] length of input arrays do not match")
+        else:
+            self.betas = env_betas
+            self.times = sim_times
+            self.alphas = alphas
+            self.env_per_iter = used_env_per_beta
+            self.sys_start_beta = sys_start_beta
+            self.ham_sys = ham_sys
+            self.ham_env_base = ham_env_base
+            self.num_monte_carlo = num_monte_carlo
+            self.verbose = verbose
+
+    def print_params(self):
+        print("betas:", self.betas)
+        print("times:", self.times)
+        print("alphas:", self.alphas)
+        print("env_per_iter:", self.env_per_iter)
+        print("sys_start_beta:", self.sys_start_beta)
+        print("ham_sys:", self.ham_sys)
+        print("ham_env:", self.ham_env_base)
+        print("num_monte_carlo:", self.num_monte_carlo)
+        print("verbose:", self.verbose)
+
+    def channel(self, rho_sys, env_beta, alpha, time):
+        if rho_sys.shape != self.ham_sys.shape:
+            if self.verbose:
+                print("[QHMC.channel] rho.shape, self.ham_sys.shape", rho_sys.shape, ",", self.ham_sys.shape)
+            return None
+        dim1, dim2 = self.ham_sys.shape[0], self.ham_env_base.shape[0] 
+        rho_env = thermal_state(self.ham_env_base, env_beta)
+        rho_tot = np.kron(rho_sys, rho_env)
+        avg_out = np.zeros((rho_sys.shape[0], rho_sys.shape[1] * 2)).view(np.complex128)
+        h = np.kron(self.ham_sys, np.identity(dim2 )) + np.kron(np.identity(dim1), self.ham_env_base)
+        for sample in range(self.num_monte_carlo):
+            g = gue(dim1 * dim2)
+            ham_tot = h + alpha * g
+            u = linalg.expm(1j * ham_tot * time)
+            raw = u @ rho_tot @ u.conj().T
+            out = partrace(raw, dim1, dim2)
+            avg_out += out.view(np.complex128) / self.num_monte_carlo
+        b, e = find_best_beta(avg_out, self.ham_sys)
+        print("b,e:", b, ", ", e)
+        return (avg_out, b, e)
+        
+        # return a dictionary mapping to a list of sys output betas and erros, do we only return average beta and avg error?
+        # or return a "list of lists" to let another function process? 
+    def compute_betas_and_errors(self):
+        if self.verbose:
+            start_time = time_this.time()
+            print("#"*75)
+            print("[compute_betas_and_errors] start time:", start_time)
+        rho = thermal_state(self.ham_sys, self.sys_start_beta)
+        ret_betas, ret_errors = [], []
+        rho_diags = []
+        for ix in range(len(self.betas)):
+            if self.verbose:
+                print("percent done:", float(ix) / len(self.betas))
+            rho, out_beta, error = self.channel(rho, self.betas[ix], self.alphas[ix], self.times[ix])
+            ret_betas.append(out_beta)
+            ret_errors.append(error)
+            rho_diags.append(np.abs(np.diagonal(rho)))
+        print("done.")
+        if self.verbose:
+            for ix in range(len(rho_diags)):
+                true = np.diagonal(thermal_state(self.ham_sys, self.betas[ix]))
+                l = "beta_env=" + str(self.betas[ix])
+                print("l:", l)
+                plt.plot(range(rho_diags[ix].shape[0]), rho_diags[ix], label=l)
+                plt.plot(range(rho_diags[ix].shape[0]), true, "--", label="true:" + str(self.betas[ix]))
+
+            plt.legend()
+            plt.show()
+
+
+
+
 
 def channel(rho, beta_env, time, alpha, dim1=10, dim2=10):
     oscillator1 = harmonic_oscillator_hamiltonian(dim1)
@@ -32,45 +149,56 @@ def channel(rho, beta_env, time, alpha, dim1=10, dim2=10):
     raw = u @ rho_tot @ u.conj().T
     return partrace(raw, dim1, dim2)
 
-def repeat_channel(beta0, betaf, dim1=10, dim2=10):
+def sample_channel(betas = [0.0, 1.0], times = [1], alphas = [0.01], used_env_per_beta = 1, ham_sysn=np.identity(2), ham_env_base=np.diag([0.5, 1.5]), verbose=False):
+    if verbose:
+        start_time = time_this.time()
+        print("#"*75)
+        print("call time:", start_time)
+        print("beta0, betaf, num_beta=10, used_env_per_beta = 100, time=100, system_hamiltonian=np.identity(2), env_dim=10, verbose=False", beta0, betaf, num_beta, used_env_per_beta, time, system_hamiltonian, env_dim, verbose)
+
     h_sys = harmonic_oscillator_hamiltonian(dim1)
     rho = thermal_state(h_sys, beta0)
-    for n in range(100):
-        beta = beta0 + (n*0.01) * betaf
-        rho = channel(rho, beta, 100, 0.01, dim1=dim1, dim2=dim2)
-    get_beta_min(rho, h_sys)
+    count = 1
+    for n in range(num_samples):
+        if n % int(num_samples / 10) == 0 and verbose:
+            print(count*10, "% complete. time since start:", time_this.time() - start_time)
+            count += 1
+        beta = 2* betaf
+        rho = channel(rho, beta, time, 0.01, dim1=dim1, dim2=dim2)
+    print("Final output:")
+    return get_beta_min(rho, h_sys)
 
 
-def get_beta_min(rho, ham):
-    dims = [Real(0.0, 1e3)]
-    def obj_fun(dim):
-        beta = dim[0]
-        guess = thermal_state(ham, beta)
-        return np.linalg.norm(rho - guess)
-    ret = gbrt_minimize(obj_fun, dims, x0=[1.0])
-    print("found beta: ", ret.x)
-    print("frobenius diff norm:", ret.fun)
+
+def test_time(t1, t2):
+    betas, errors = [], []
+    count = 0
+    for t in np.geomspace(t1, t2):
+        print("*" * 75)
+        print("count:", count)
+        count += 1
+        beta, error = repeat_channel()
+        betas.append(beta)
+        errors.append(error)
+    return (betas, errors)
+
+def test_samples(s1, s2, num_steps=50, beta_0=0.0, beta_1=1.0, time=100, env_dim=10, sys_ham=np.zeros(1) ):
+    betas, errors = [], []
+    count = 0
+    for samp in np.geomspace(s1, s2, num=num_steps):
+        print("~" * 75)
+        print("count:", count)
+        count += 1
+        b, e = repeat_channel(beta_0, beta_1, time, sys_ham, env_dim)
 
 def test():
-    d1 = 4
-    d2 = 8
-    b1 = 0.
-    b2 = 1.
-    t = 100.
-    alpha = 0.1
-    h1 = harmonic_oscillator_hamiltonian(d1)
-    therm1 = thermal_state(h1, b1)
-    out = channel(therm1, b2, t, alpha, dim1=d1, dim2=d2)
-    print("energy 1:", np.trace(h1 @ therm1))
-    print("oscillator 1")
-    print(h1)
-    print("output")
-    print(out)
-    print("energy out:", np.trace(out @ h1))
-    print("#" * 50)
-    print("output diagonals:", np.abs(np.diagonal(out)))
-    print("input thermal state:", np.abs(np.diagonal(therm1)))
-    print("delta:", np.abs(np.diagonal(out)) - np.abs(np.diagonal(therm1)))
-    get_beta_min(out, h1)
-    repeat_channel(0.0, 1.0)
+    h1 = harmonic_oscillator_hamiltonian(10)
+    h2 = harmonic_oscillator_hamiltonian(10)
+    betas = [1., 10., 100.]
+    times = [100] * 3
+    alphas = [0.05] * 3
+    qhmc = QHMC(ham_sys = h1, ham_env_base=h2, env_betas=betas, sim_times=times, alphas=alphas, verbose=True)
+    qhmc.print_params()
+    qhmc.compute_betas_and_errors()
+
 test()
