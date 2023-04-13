3549917fea4dc8012007224322fe1bb82a322638
Matthew Hagan <matthewhagan15@gmail.com>
2023-04-13 18:20:32 -0400

    minor

--- numerics/alpha_analysis.py
+++ numerics/alpha_analysis.py
@@ -1,12 +1,23 @@
 #!/usr/bin/env python
+import json
+import matplotlib.pyplot as plt
 
 base_dir = "/scratch/n/nawiebe/hagan/tsp/error_v_interaction/"
 datasets = ["alpha_0_005/1", "alpha_0_01/correct_data", "alpha_0_05/1", "alpha_0_10/1"]
-for data_file in datasets:
-    with open(base_dir + data_file, 'r') as f:
+alphas = [0.005, 0.01, 0.05, 0.1]
+interactions = range(1,101)
+means = {}
+stds = {}
+for ix in range(len(datasets)):
+    with open(base_dir + datasets[ix], 'r') as f:
         json_string = f.read()
         x = json.loads(json_string)
-        print("file: ", base_dir + data_file)
+        mean_data = []
+        std_data = []
         for k,v in x.items():
-            print("k = ", k)
-            print("v = ", v)
+            mean_data.append((k, v[1]))
+            std_data.append((k, v[2]))
+        means[alphas[ix]] = mean_data
+        stds[alphas[ix]] = std_data
+fig = plt.figure()
+for alpha in alphas
