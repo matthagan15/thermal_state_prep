a00773be62ef76f5db6f3f627642d8ce7cd1d8ea
Matthew Hagan <matthewhagan15@gmail.com>
2024-06-14 15:08:13 -0400

    single_shot python analysis started

---
+++ numerics/analysis/single_shot.py
@@ -0,0 +1,43 @@
+import json
+from os import walk
+from os.path import isfile, join
+import matplotlib.pyplot as plt
+from math import sqrt
+import numpy as np
+
+filepath = "/Users/matt/repos/thermal_state_prep/numerics/data/single_shot_harmonic/results.json"
+
+
+with open(filepath) as f:
+    j = json.load(f)
+    inputs = j["inputs"]
+    outputs = j["outputs"]
+    num_samples = j["num_samples"]
+    dim_sys = j["dim_sys"]
+    label = j["label"]
+    if len(inputs) != len(outputs):
+        raise Exception("inputs and outputs are not the same length.")
+    
+    variable = "beta_env"
+
+    if variable == "beta_env":
+        alpha_0 = inputs[0][0]
+        beta_sys_0 = inputs[0][2]
+        time_0 = inputs[0][3]
+
+        x = []
+        y = []
+        yerr = []
+        for ix in range(len(inputs)):
+            (alpha, beta_env, beta_sys, time) = inputs[ix]
+            (original_dist, mean_dist, std_dist, dist_of_mean) = outputs[ix]
+            if alpha == alpha_0 and beta_sys == beta_sys_0 and time == time_0:
+                x.append(beta_env)
+                y.append(mean_dist)
+                yerr.append(std_dist)
+            
+        fig = plt.figure()
+        plt.errorbar(x, y, yerr)
+        plt.show()
+        
+    
