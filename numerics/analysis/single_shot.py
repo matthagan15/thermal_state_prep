9d23184a884fbba36a2c96686d26280c78cbf6de
Matthew Hagan <matthewhagan15@gmail.com>
2024-06-14 22:55:51 -0400

    multiple plots single_shot

--- numerics/analysis/single_shot.py
+++ numerics/analysis/single_shot.py
@@ -21,23 +21,27 @@ with open(filepath) as f:
     variable = "beta_env"
 
     if variable == "beta_env":
-        alpha_0 = inputs[0][0]
-        beta_sys_0 = inputs[0][2]
-        time_0 = inputs[0][3]
-
-        x = []
-        y = []
-        yerr = []
-        for ix in range(len(inputs)):
-            (alpha, beta_env, beta_sys, time) = inputs[ix]
-            (original_dist, mean_dist, std_dist, dist_of_mean) = outputs[ix]
-            if alpha == alpha_0 and beta_sys == beta_sys_0 and time == time_0:
-                x.append(beta_env)
-                y.append(mean_dist)
-                yerr.append(std_dist)
-            
+        # The format of this is [(alpha, beta_env, time)]
+        fixed_params = [(inputs[0][0], inputs[0][1], inputs[0][3]), (inputs[10][0], inputs[10][1], inputs[10][3])]
         fig = plt.figure()
-        plt.errorbar(x, y, yerr)
+        colors = ['r', 'b', 'g', 'black']
+        color_count = 0
+        for (alpha_0, beta_env_0, time_0) in fixed_params:
+            print("alpha, beta_env, time", alpha_0, beta_env_0, time_0)
+            x = []
+            y = []
+            yerr = []
+            for ix in range(len(inputs)):
+                (alpha, beta_env, beta_sys, time) = inputs[ix]
+                (original_dist, mean_dist, std_dist, dist_of_mean) = outputs[ix]
+                if alpha == alpha_0 and beta_env == beta_env_0 and time == time_0:
+                    x.append(beta_sys)
+                    y.append(mean_dist)
+                    yerr.append(std_dist)
+
+                    print("beta_sys, mean_dist = ", beta_sys, ", ", mean_dist)
+            plt.errorbar(x, y, yerr, marker='x', linestyle='none', label="a={:.2f}, b_e={:.2f}, t={:.2f}".format(alpha_0, beta_env_0, time_0), color=colors[color_count])
+            color_count += 1
         plt.show()
         
     
