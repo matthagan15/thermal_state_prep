5815bb9180c1e7f43b613fe1069a653440858f46
Matthew Hagan <matthewhagan15@gmail.com>
2024-09-17 16:19:50 -0400

    data

--- numerics/src/fixed_epsilon.rs
+++ numerics/src/fixed_epsilon.rs
@@ -11,6 +11,26 @@ use crate::{
     generate_floats, harmonic_oscillator_gaps, HamiltonianType,
 };
 
+fn binary_search<F>(f: F, epsilon: f64) -> usize
+where
+    F: Fn(usize) -> f64,
+{
+    let mut lower = 1;
+    let mut upper = 1;
+    while f(upper) > epsilon {
+        upper *= 2;
+    }
+    while (upper - lower) > 1 {
+        let mid = (lower + upper) / 2;
+        if f(mid) >= epsilon {
+            lower = mid;
+        } else {
+            upper = mid;
+        }
+    }
+    upper
+}
+
 pub fn run(config_file: &Path, results_file: &Path, label: String) -> MultiShotResults {
     let conf = FixedEpsilonConfig::from_file(config_file);
     let alphas = generate_floats(
@@ -38,8 +58,9 @@ pub fn run(config_file: &Path, results_file: &Path, label: String) -> MultiShotR
         conf.epsilon_logspace,
     );
     let h_sys = conf.hamiltonian_type.as_ndarray(conf.dim_sys);
-    let oscillator_gaps = harmonic_oscillator_gaps(conf.dim_sys);
-    let gamma_strat = GammaStrategy::known(oscillator_gaps, 100);
+    // let oscillator_gaps = harmonic_oscillator_gaps(conf.dim_sys);
+    // let gamma_strat = GammaStrategy::known(oscillator_gaps, 100);
+    let gamma_strat = conf.gamma_strategy;
     let mut inputs = Vec::new();
     let mut outputs = Vec::new();
     for alpha in alphas.iter() {
@@ -48,15 +69,16 @@ pub fn run(config_file: &Path, results_file: &Path, label: String) -> MultiShotR
                 for epsilon in epsilons.iter() {
                     println!("{:}", "*".repeat(50));
                     println!("inputs: {:}, {:}, {:}, {:}", alpha, beta_env, epsilon, time);
-                    let mut current_distance = f64::MAX;
-                    let mut num_interactions = 0;
                     let (mut mean_dist, mut std_dist, mut dist_of_avg) = (0.0, 0.0, 0.0);
-                    while current_distance > *epsilon {
-                        println!("num interactions: {:}", num_interactions);
-                        println!("current distance: {:}", current_distance);
-                        num_interactions += 1;
+                    let interactions_to_epsilon = |num_interactions: usize| {
                         let phi_alphas = vec![*alpha; num_interactions];
-                        let phi_beta_envs = vec![*beta_env; num_interactions];
+                        // let phi_beta_envs = vec![*beta_env; num_interactions];
+                        let phi_beta_envs = ndarray::Array::linspace(
+                            beta_env / num_interactions as f64,
+                            *beta_env,
+                            num_interactions,
+                        )
+                        .to_vec();
                         let phi_times = vec![*time; num_interactions];
                         let phi = IteratedChannel::new(
                             h_sys.clone(),
@@ -69,11 +91,40 @@ pub fn run(config_file: &Path, results_file: &Path, label: String) -> MultiShotR
                         if phi_outputs.len() == 0 {
                             panic!("Simulator should have returned at least one data point.");
                         }
-                        (mean_dist, std_dist, dist_of_avg) = phi_outputs[phi_outputs.len() - 1];
-                        current_distance = mean_dist;
-                    }
+                        let (mean_dist, std_dist, dist_of_avg) = phi_outputs[phi_outputs.len() - 1];
+                        mean_dist
+                    };
+                    let interactions_needed = binary_search(interactions_to_epsilon, *epsilon);
+                    println!("needed {interactions_needed} interactions.");
+                    // while current_distance > *epsilon {
+                    //     println!("num interactions: {:}", num_interactions);
+                    //     println!("current distance: {:}", current_distance);
+                    //     num_interactions += 1;
+                    //     let phi_alphas = vec![*alpha; num_interactions];
+                    //     // let phi_beta_envs = vec![*beta_env; num_interactions];
+                    //     let phi_beta_envs = ndarray::Array::linspace(
+                    //         beta_env / num_interactions as f64,
+                    //         *beta_env,
+                    //         num_interactions,
+                    //     )
+                    //     .to_vec();
+                    //     let phi_times = vec![*time; num_interactions];
+                    //     let phi = IteratedChannel::new(
+                    //         h_sys.clone(),
+                    //         phi_alphas,
+                    //         phi_beta_envs,
+                    //         phi_times,
+                    //         gamma_strat.clone(),
+                    //     );
+                    //     let phi_outputs = phi.simulate(0.0, *beta_env, conf.num_samples);
+                    //     if phi_outputs.len() == 0 {
+                    //         panic!("Simulator should have returned at least one data point.");
+                    //     }
+                    //     (mean_dist, std_dist, dist_of_avg) = phi_outputs[phi_outputs.len() - 1];
+                    //     current_distance = mean_dist;
+                    // }
                     inputs.push((*alpha, *beta_env, *epsilon, *time));
-                    outputs.push((num_interactions, mean_dist, std_dist, dist_of_avg))
+                    outputs.push((interactions_needed, mean_dist, std_dist, dist_of_avg))
                 }
             }
         }
